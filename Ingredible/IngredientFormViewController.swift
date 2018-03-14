23f87fc66d400bc09c0a36c92c3511028e0bb351
Kayla Nahi <kk@nahi.net>
2018-03-14 15:25:21 -0400

    With ingredient form VC.

---
+++ Ingredible/IngredientFormViewController.swift
@@ -0,0 +1,39 @@
+//
+//  IngredientFormViewController.swift
+//  Ingredible
+//
+//  Created by Kayla Nahi on 3/14/18.
+//  Copyright © 2018 Ingredible. All rights reserved.
+//
+
+import UIKit
+
+class IngredientFormViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func backButton(_ sender: Any) {
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
